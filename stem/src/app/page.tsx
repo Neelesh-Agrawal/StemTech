ba5e7d669d582f0f66b27a9900a9dac0af9e0e22
Kedar <kedarshinde8011@gmail.com>
2025-06-15 23:11:31 +0530

    final code

--- stem/src/app/page.tsx
+++ stem/src/app/page.tsx
@@ -1,8 +1,9 @@
 import Desktop from './components/Desktop';
+import styles from './styles/index.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.desktop11}>
       <Desktop />
     </div>
   );
